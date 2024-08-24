13bc3b621f582db3c5c620445913cba758cf80b8
Adekola Akano <akano.adekola@gmail.com>
2024-08-24 09:19:00 +0100

    Added plugin to new menu option

--- src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/CleanArchitectureTemplateName.kt
+++ src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/CleanArchitectureTemplateName.kt
@@ -23,23 +23,17 @@ class CleanArchitectureTemplateName(private val event: AnActionEvent) : DialogWr
 
     private lateinit var panel: JPanel
 
-    private val inputTextArea = JTextArea(5, 50)
-    private val resultTextArea = JTextArea(3, 50)
-    private val submitButton = JButton("Submit")
-
-    private val classNameText: JLabel = JLabel()
-    private val classNameTextField: JTextField = JTextField()
-
-    private val methodNameText: JLabel = JLabel()
-    private val methodNameTextField: JTextField = JTextField()
+    private val featureNameTextField: JTextField = JTextField()
+    private val responseNameTextField: JTextField = JTextField()
+    private val bodyNameTextField: JTextField = JTextField()
 
     private val javaRadioButton: JRadioButton = JRadioButton("Java")
+    private val flutterRadioButton: JRadioButton = JRadioButton("Flutter")
     private val kotlinRadioButton: JRadioButton = JRadioButton("Kotlin", true)
     private val languageGroup: ButtonGroup = ButtonGroup()
 
-    private val copyButton = JButton("Copy to Clipboard")
-    private val addToExistingButton = JButton("Add to existing class")
-    private val createClassButton = JButton("Create new class")
+    private val featureNameText: JLabel = JLabel()
+    private val createClassButton = JButton("Create ")
 
     init {
         init()
@@ -52,47 +46,19 @@ class CleanArchitectureTemplateName(private val event: AnActionEvent) : DialogWr
         panel = JPanel(GridBagLayout())
         val constraints = GridBagConstraints()
 
-        inputTextArea.text = ""
 
-        constraints.fill = GridBagConstraints.HORIZONTAL
-        constraints.gridx = 0
-        constraints.weightx = 1.0
-        constraints.insets = JBUI.insets(4)
-        constraints.anchor = GridBagConstraints.NORTH
-
-
+        featureNameText.text = "Class Name"
         constraints.gridy += 1
-        constraints.weighty = 0.0
-        panel.add(JScrollPane(inputTextArea), constraints)
+        panel.add(featureNameText, constraints)
 
-        methodNameText.text = "Method Name"
+        featureNameTextField.text = "ApiService"
+        featureNameTextField.preferredSize = Dimension(250, 30)
         constraints.gridy += 1
-        panel.add(methodNameText, constraints)
-
-        methodNameTextField.text = "performRequest"
-        methodNameTextField.preferredSize = Dimension(250, 30)
-        constraints.gridy += 1
-        panel.add(methodNameTextField, constraints)
-
-        constraints.gridy += 1
-        panel.add(submitButton, constraints)
-
-        constraints.gridy += 1
-
-        resultTextArea.isEditable = false
-        panel.add(JScrollPane(resultTextArea), constraints)
-
-        classNameText.text = "Class Name"
-        constraints.gridy += 1
-        panel.add(classNameText, constraints)
-
-        classNameTextField.text = "ApiService"
-        classNameTextField.preferredSize = Dimension(250, 30)
-        constraints.gridy += 1
-        panel.add(classNameTextField, constraints)
+        panel.add(featureNameTextField, constraints)
 
         languageGroup.add(kotlinRadioButton);
         languageGroup.add(javaRadioButton);
+        languageGroup.add(flutterRadioButton);
 
         constraints.gridy += 1
         panel.add(javaRadioButton, constraints)
@@ -101,8 +67,6 @@ class CleanArchitectureTemplateName(private val event: AnActionEvent) : DialogWr
 
 
         val buttonPanel = JPanel(FlowLayout(FlowLayout.RIGHT))
-        buttonPanel.add(copyButton)
-        buttonPanel.add(addToExistingButton)
         buttonPanel.add(createClassButton)
         constraints.gridy += 1
         constraints.weighty = 0.0
--- src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/CreateCleanArchitectureTemplate.kt
+++ src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/CreateCleanArchitectureTemplate.kt
@@ -2,10 +2,22 @@ package com.github.dekola.androidcleanarchitecturetemplateplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+//class CreateCleanArchitectureTemplate : AnAction() {
+//    override fun actionPerformed(event: AnActionEvent) {
+//        CleanArchitectureTemplateName(event).show()
+//    }
+//
+//}
 
 class CreateCleanArchitectureTemplate : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        CleanArchitectureTemplateName(event).show()
+        // For demonstration purposes, just show a dialog
+        Messages.showMessageDialog(
+            "Clean Architecture Template Created!",
+            "Information",
+            Messages.getInformationIcon()
+        )
     }
-
 }
