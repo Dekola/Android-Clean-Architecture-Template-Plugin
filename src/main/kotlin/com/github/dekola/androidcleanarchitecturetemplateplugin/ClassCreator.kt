f95f45901e60b0ec1d7f480bdfe4ea93e8a25beb
Adekola Akano <akano.adekola@gmail.com>
2024-09-17 19:57:29 +0100

    Added directory creation

--- src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/ClassCreator.kt
+++ src/main/kotlin/com/github/dekola/androidcleanarchitecturetemplateplugin/ClassCreator.kt
@@ -10,15 +10,43 @@ object ClassCreator {
 
     fun createTemplateStructure(
         project: Project,
-        folder: PsiDirectory?,
-        featureName: String?,
+        baseDirectory: PsiDirectory?,
+        featureName: String,
         selectedLanguage: LanguageSelection,
         packageName: String
     ) {
 
 //        val directory = PsiManager.getInstance(project)
-//            .findDirectory(project.baseDir)
+//            baseDirectory.findDirectory(project.baseDir)
 
+
+
+
+        val baseFeatureDirectory = createDirectory(project, baseDirectory, featureName)
+
+        val viewModelDirectory = createDirectory(project, baseFeatureDirectory, "UI")
+        val dataDirectory = createDirectory(project, baseFeatureDirectory, "data")
+        val repositoryDirectory = createDirectory(project, dataDirectory, "repository")
+        val remoteDataSourceDirectory = createDirectory(project, dataDirectory, "remoteDataSource")
+        val apiDirectory = createDirectory(project, dataDirectory, "api")
+
+        val viewModelCode:String = CodeGeneratorClass.getViewModelCode(selectedLanguage, baseDirectory, featureName, packageName)
+        val repositoryCode:String = CodeGeneratorClass.getRepositoryCode(selectedLanguage, baseDirectory, featureName)
+        val remoteDataSourceCode:String = CodeGeneratorClass.getRemoteDataSourceCode(selectedLanguage, baseDirectory, featureName)
+        val apiInterfaceCode:String = CodeGeneratorClass.getApiInterfaceCode(selectedLanguage, baseDirectory, featureName)
+
+        createClass(project, viewModelDirectory, selectedLanguage, featureName, "ViewModel", viewModelCode)
+
+    }
+
+    fun createClass(
+        project: Project,
+        viewModelDirectory: PsiDirectory?,
+        selectedLanguage: LanguageSelection,
+        featureName: String,
+        classType: String,
+        viewModelCode: String
+    ) {
         val suffix = when (selectedLanguage) {
             LanguageSelection.JAVA -> {
                 ".java"
@@ -26,23 +54,34 @@ object ClassCreator {
             LanguageSelection.KOTLIN -> ".kt"
             else -> ".dart"
         }
-
-        val viewModelCode:String = CodeGeneratorClass.getViewModelCode(selectedLanguage, folder, featureName, packageName)
-        val repositoryCode:String = CodeGeneratorClass.getRepositoryCode(selectedLanguage, folder, featureName)
-        val remoteDataSourceCode:String = CodeGeneratorClass.getRemoteDataSourceCode(selectedLanguage, folder, featureName)
-        val apiInterfaceCode:String = CodeGeneratorClass.getApiInterfaceCode(selectedLanguage, folder, featureName)
-
-
-        WriteCommandAction.runWriteCommandAction(project) {
-            val fileType = FileTypeManager.getInstance().getFileTypeByExtension("kt")
-            val psiFile = PsiFileFactory.getInstance(project)
-                .createFileFromText("${featureName}ViewModel$suffix", fileType, viewModelCode)
-            folder?.add(psiFile)
-
+        val extension = when (selectedLanguage) {
+            LanguageSelection.JAVA -> {
+                "java"
+            }
+            LanguageSelection.KOTLIN -> "kt"
+            else -> "dart"
         }
-
+        WriteCommandAction.runWriteCommandAction(project) {
+            val fileType = FileTypeManager.getInstance().getFileTypeByExtension(extension)
+            val psiFile = PsiFileFactory.getInstance(project)
+                .createFileFromText("${featureName}$classType$suffix", fileType, viewModelCode)
+            viewModelDirectory?.add(psiFile)
+        }
     }
 
+    fun createDirectory(project: Project, baseDirectory: PsiDirectory?, directoryName: String): PsiDirectory? {
+        var newSubdirectory: PsiDirectory? = null
+
+        WriteCommandAction.runWriteCommandAction(project) {
+            try {
+                newSubdirectory = baseDirectory?.createSubdirectory(directoryName)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        return newSubdirectory
+    }
 
 
 
